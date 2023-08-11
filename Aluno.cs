80e42e966d9ccc45520b7846e8280e74da5b7cca
Almir Rogério Camolesi <camolesi@fema.edu.br>
2023-08-11 11:35:45 -0300

    Add project files.

---
+++ Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APITAT
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        public string nome { get; set; }
+
+        [StringLength(30)]
+        public string disciplina { get; set; }
+
+        public float nota1 { get; set; }
+        public float nota2 { get; set; }
+        
+    }
+}
